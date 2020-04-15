6cec43f50112ebe0013d8f2e5b6e20c455b8997f
nouserename <48287164+nouserename@users.noreply.github.com>
2020-04-15 16:26:15 +0800

    finish experiment 17

--- controller/v1/captchas.js
+++ controller/v1/captchas.js
@@ -3,15 +3,15 @@
 import captchapng from 'captchapng';
 
 class Captchas {
-	constructor(){
+    constructor(){
 
-	}
-	//验证码
-	async getCaptchas(req, res, next){
+    }
+    //验证码
+    async getCaptchas(req, res, next){
         //随机四位数字
         const cap = parseInt(Math.random()*9000+1000);
         //创建验证码图片
-    	const p = new captchapng(80,30, cap);
+        const p = new captchapng(80,30, cap);
         p.color(0, 0, 0, 0); 
         p.color(80, 80, 80, 255);
         //转为 64位编码格式
@@ -21,9 +21,9 @@ class Captchas {
         //返回一张 64位编码图片 给前端
         res.send({
             status: 1,
-        	code: 'data:image/png;base64,' + base64
+            code: 'data:image/png;base64,' + base64
         });
-	}
+    }
 }
 //创建实例
 export default new Captchas()
